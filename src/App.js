763ec0f27d027316f82d83d9c9095e4fd487f918
mitchdmarino <mitchdmarino@gmail.com>
2023-05-21 20:44:31 -0700

    Finished main styles

--- src/App.js
+++ src/App.js
@@ -4,9 +4,19 @@ import Board from './components/Board/Board';
 function App() {
   return (
     <div className="App">
-      <div>
-        <h1>Webtoon Bingo</h1>
-        <p>Tap on a square to add a picture.</p>
+      <div className='header'>
+        <img className='logo' alt="webtoon bingo"  src="/images/webtoon-bingo.png"/>
+        <p>Hello! <br></br><br></br>
+
+          We are playing WEBTOON Bingo, but in the office! <br></br><br></br>
+
+          Complete these special in-office tasks for 5 bingo squares to win “BINGO” and a special prize.  For your square to count, you need to take a photo documenting that you’ve done the bingo square’s task. To start, tap on a bingo square to add a photo.  <br></br><br></br>
+
+          Please note: this site does not save your uploaded images or information. So, if you accidentally refresh the page, you will not have your Bingo squares’ photos saved and will have to re-add them. We recommend to snap and save the bingo square photos onto your camera roll first, then upload them onto the bingo square once you have 5 bingo squares ready to go! <br></br><br></br>
+
+          Once you have uploaded all 5 bingo square photos and know you’ve gotten bingo in a row, share this on #wt-officebingo Slack channel and Dija will check! First person to get bingo wins a prize! For any issues, contact Dija. <br></br><br></br>
+
+          Good luck!</p>
       </div>
 
       <div className="main">
